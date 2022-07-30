0163a91f43d12bb699f4c4794324fd3fa04d4764
sasik <sasikaranselvaraj@gmail,com>
2022-07-30 22:53:50 +0530

    "Home work done on 29.07.2022"

--- StudentsWork/Homework_SS.py
+++ StudentsWork/Homework_SS.py
@@ -40,4 +40,3 @@ Tip1 = int(input("Enter the Tips"))
 Tip2 = (Bill * Tip1/100) + Bill
 print('The %.2f is Bill Amount  %d is Tip Amount amd %d is Total Amount  ' % (Bill, Tip1, Tip2))
 
-'home work'
