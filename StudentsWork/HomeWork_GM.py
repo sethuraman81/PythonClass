0ad0b2cc850e43c6a80116c822ad1982350286a8
Krithiga09 <8bkrithigashri@gmail.com>
2022-08-01 14:17:48 +0530

    HomeWork By GM

--- StudentsWork/HomeWork_GM.py
+++ StudentsWork/HomeWork_GM.py
@@ -1,1 +1,8 @@
-print("Hello World -- Mahendran Second change")
+#Given _a Temperature number in Celsius convert the value to Fahrenheit and print Both Celsius and Fahrenhiet values
+#Formula is : (1°C × 9/5) + 32 = 33.8°F
+#C- is celsius
+#F - is fahrenheit
+
+Celcs = int(input("Enter the value for Celcious: "))
+Faht = (Celcs * (9/5) + 32)
+print("Celcious Value given is {0}" .format(Celcs) + " \n Fahranheat value is {0}" .format(Faht))
