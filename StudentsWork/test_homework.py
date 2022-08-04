b8491fbea8bc4f82282fe2b1744715cab4ba309a
Ezhilraj K <ezhil83raj@gmail.com>
2022-08-04 21:33:09 +0530

    Test HW Submitted

--- StudentsWork/test_homework.py
+++ StudentsWork/test_homework.py
@@ -1,10 +1,2 @@
 "Test content to work on"
 "Second set of changes"
-
-def func_args2(arg1,*args,arg2):
-    print('arg1', arg1)
-    print('args', *args)
-    print('arg2', arg2)
-
-func_args("Hello World",1,2,3,4,5,arg2 = 12)
-    return(func_args2(arg1,*args,arg2))
