056ca3572bc2c4f2ad08c6a6a5046cff5f9461b9
Sethu Raman <sethuramanmca@gmail.com>
2022-08-04 16:52:44 +0100

    Python Book to share

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-import datetime
-a=None
-a = a or datetime.datetime.now()
-print(a)
+def mult(*a):
+ for i in a:
+  c=c*i
 
+#Kw pair
+#lambda
+#Classes - Objects
+#selenium
+
+def add(v1, *, v2,v3=0):
+    print('arg1: ', v2)
+    print('arg2: ', v3)
+
+add(35,v2=45,v3=56)
+
+print("Calling add function " ,add(3,v2=4))
