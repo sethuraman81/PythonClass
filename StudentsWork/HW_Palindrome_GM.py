38970a49c039614a4a29d7d0fdd87e37535e4016
Sethu Raman <sethuramanmca@gmail.com>
2022-08-19 11:34:16 +0100

    merge with changes

--- StudentsWork/HW_Palindrome_GM.py
+++ StudentsWork/HW_Palindrome_GM.py
@@ -1,13 +1,16 @@
 #To find Palindrome for number from 1 to 1000
 
-for a in range(1,1000):
+for a in range(1,1000): #500
     if(a>10):
-        for j in range(1,3):
-            b = str(a)
-            lent = len(b)
-            len1 = lent%2
+        for j in range(1,3):#2
+            b = str(a) #555
+            lent = len(b) #3
+            len1 = lent%2 #1
             if len1 == 1:
-                len2 = len1//2 +1
+                len2 = len1//2 +1 #1
+                #555 == 555
+                #55 == 55
+                #555 == 555
                 if(b[::j]==b[::-j] and b[::len2]==b[::1]):
                     print(b)
 
